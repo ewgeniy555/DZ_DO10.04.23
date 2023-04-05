e4e1c7e1d1ee109e31cada8323abd0045936ebde
ewgeniy212 <ewgeniy212@mail.ru>
2023-04-06 01:16:12 +0800

    заставил работать

--- Program.cs
+++ Program.cs
@@ -4,7 +4,9 @@
         int index =0;
 		for(int i =0;i<str.Length/3;i++){	
 		 for(int j=0;j<3;j++){
-			
+			a=a.Insert(j,str[index].ToString());//заталкивае символы по одному
+           ar[i]=a;
+           
 		 }
         a="";//очищает строку
 		// Console.WriteLine(ar[i]);
