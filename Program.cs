1d171608d2d6399d242364b65d5a2f88db883a8a
ewgeniy212 <ewgeniy212@mail.ru>
2023-04-06 01:14:29 +0800

    создал цилы

--- Program.cs
+++ Program.cs
@@ -1,5 +1,12 @@
-﻿         string a="";
+﻿        string a="";
 		string str= "Привет как дела у вас всё хорошо?  ";
 		string[]ar = new string[str.Length/3];
         int index =0;
-		
+		for(int i =0;i<str.Length/3;i++){	
+		 for(int j=0;j<3;j++){
+			
+		 }
+        a="";//очищает строку
+		// Console.WriteLine(ar[i]);
+		}
+	
